dba60a77e3a9fb8f999d7a5f026cd3a2d29cb668
Ivisson César <ivissoncesar@gmail.com>
2025-01-23 16:29:36 -0300

    Adicionado validação para espaço vazio e adiciona na lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    if (document.querySelector('input').value == "") {
+        alert('Por favor, insira um nome válido')
+    } else {
+        let inserirNome = document.querySelector('input').value;
+        amigos.push(inserirNome);
+        console.log(amigos);
+    }
+    
+
+    // function sortearAmigo(){
+    //    return parseInt(Math.random() * amigos.length + 1)
+    // }
+}
